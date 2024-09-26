8f558bbf48b80c3500442b2133bb6f2682c67080
Aslah7070 <aslah.c7070@gmail.com>
2024-09-26 15:08:11 +0530

    string method started

---
+++ string methods/slice.js
@@ -0,0 +1,6 @@
+let str="my name is aslah";
+console.log("one",str.slice(2,6));
+console.log("2 :",str.slice(-9));
+console.log("3 :",str.slice(1,3));
+console.log("4 :",str.slice(6,9));
+console.log("5 :",str.slice(6,3));
---
+++ string methods/substring.js
@@ -0,0 +1,6 @@
+let str="my name is aslah";
+console.log("1",str.substring(1,4));
+console.log("2",str.substring(1,3));
+console.log("3",str.substring(1,3));
+console.log("4",str.substring(6,3));
+
